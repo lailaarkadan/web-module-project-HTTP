7a2926c9f434a5a7d1817b1615824c8422fee19a
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-12-15 14:43:49 -0700

    working on EditMovie

--- src/components/EditMovieForm.js
+++ src/components/EditMovieForm.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 const EditMovieForm = (props) => {
 	const { push } = useHistory();
+	const { id } = useParams();
 
 	const { setMovies } = props;
 	const [movie, setMovie] = useState({
@@ -32,11 +33,22 @@ const EditMovieForm = (props) => {
         });
     }
 
+	useEffect(()=>{
+		axios.get(`http//localhost/9000/api/movies/${id}`)
+		.then(res => {
+			setMovie(res.data);
+		})
+		.catch(err => {
+			console.log(err)
+		})
+	}, [id])
+	
+
     const handleSubmit = (e) => {
         e.preventDefault();
         axios.put(`http://localhost:9000/api/movies/${id}`, movie)
             .then(res=>{
-                setMovies(res.data);
+                props.setMovies(res.data);
                 push(`/movies/${movie.id}`);
 			})
 			.catch(err=>{
